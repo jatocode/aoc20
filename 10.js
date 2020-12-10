af55b4a9e6a5fc26b7855825ce9da9ea6f22380a
Tobias Jansson <Tobias.Jansson@prevas.se>
2020-12-10 11:12:16 +0100

    Dag 10 del 1

--- 10.js
+++ 10.js
@@ -1,8 +1,28 @@
 // Testar med ett eget utilbibliotek
 const util = require('./util.js');
 
-let data = util.getInputTrim();
+let data = util.getInputTrim().map(x => parseInt(x));
 
-data.forEach((line,i) => {
-    console.log(i,line);
-});
+// Del 1
+console.log(findsteps(data));
+
+// Del 2
+
+function findsteps(data) {
+    let end = 0;
+    let onestep = 0;
+    let threestep = 0;
+    let sorted = data.sort((a, b) => a - b);
+    for (var i = 0; i < sorted.length; i++) {
+        let jolt = sorted[i];
+        if (end <= jolt + 3) {
+            if (jolt - end == 1) onestep++;
+            if (jolt - end == 3) threestep++;
+            end = jolt;
+        }
+    }
+
+    // My adapter is always 3 jolts up
+    threestep++;
+    return onestep * threestep;
+}
