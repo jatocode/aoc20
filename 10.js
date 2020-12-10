860c24c717a94dfb01abe518db38e3359f2e7b4d
Tobias Jansson <Tobias.Jansson@prevas.se>
2020-12-10 23:27:15 +0100

    10, del 2 till slut...

--- 10.js
+++ 10.js
@@ -1,18 +1,21 @@
 // Testar med ett eget utilbibliotek
 const util = require('./util.js');
 
-let data = util.getInputTrim().map(x => parseInt(x));
+let data = util.getInputTrim().map(x => parseInt(x)).sort((a, b) => a - b);
+// Sätt in start och mål
+data.unshift(0);
+data.push(data[data.length - 1] + 3);
 
 // Del 1
 console.log(findsteps(data));
-
+console.table(data);
 // Del 2
+console.log(findpaths(data));
 
-function findsteps(data) {
+function findsteps(sorted) {
     let end = 0;
     let onestep = 0;
     let threestep = 0;
-    let sorted = data.sort((a, b) => a - b);
     for (var i = 0; i < sorted.length; i++) {
         let jolt = sorted[i];
         if (end <= jolt + 3) {
@@ -21,8 +24,18 @@ function findsteps(data) {
             end = jolt;
         }
     }
-
-    // My adapter is always 3 jolts up
-    threestep++;
     return onestep * threestep;
+}
+
+function findpaths(data) {
+    let chains = data.map((x, i) => (i == 0 ? 1 : 0));
+    for (var i = 0; i < data.length; i++) {
+        for (var j = 0; j < i; j++) {
+            if (data[i] - data[j] <= 3) {
+                chains[i] += chains[j];
+            }
+        }
+    }
+    console.table(chains);
+    return chains[data.length - 1];
 }
