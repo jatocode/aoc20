68e623e5857770cb59ee06863ba08eb4d1f2d775
Tobias Jansson <Tobias.Jansson@prevas.se>
2020-12-11 11:03:58 +0100

    dag 11 del 2

--- 11.js
+++ 11.js
@@ -1,39 +1,40 @@
 // Testar med ett eget utilbibliotek
+const { platform } = require('os');
 const util = require('./util.js');
 
 let data = util.getInputTrim();
 let seats = [];
 data.forEach(d => seats.push(d.trim().split('')));
 
-// Test
-//print();
-// console.log();
-// console.log(getNeigbours(2, 2).join(''));
-// console.log(getNeigbours(0, 0).join(''));
-// console.log(getNeigbours(9, 9).join(''));
-//console.log(getNeigbours(0,1).join(''));
-//console.log(seatState(0,1));
-
-model();
+// Del 1
+model(del1rules);
 console.log(countOccupied());
 
-function model() {
+// Reset for del 2
+seats = [];
+data.forEach(d => seats.push(d.trim().split('')));
+
+// Del 2
+model(del2rules);
+console.log(countOccupied());
+
+function model(ruleset) {
     let seatsChanged = 0;
     do {
         let newmodel = [];
         seatsChanged = 0;
         for (var y = 0; y < seats.length; y++) {
             for (var x = 0; x < seats[0].length; x++) {
-                if(newmodel[y] == undefined) newmodel[y] = [];
-                newmodel[y][x] = seatState(x, y);
+                if (newmodel[y] == undefined) newmodel[y] = [];
+                newmodel[y][x] = ruleset(x, y);
                 if (get(x, y) != newmodel[y][x]) seatsChanged++;
             }
         }
         seats = JSON.parse(JSON.stringify(newmodel));
-    } while(seatsChanged > 0)
+    } while (seatsChanged > 0)
 }
 
-function seatState(x, y) {
+function del1rules(x, y) {
     let nb = getNeigbours(x, y);
     let occ = nb.filter(x => x == '#').length;
     let seat = get(x, y);
@@ -45,12 +46,46 @@ function seatState(x, y) {
     return seat;
 }
 
+function del2rules(x, y) {
+    let occ = getLineOcc(x,y, [0,-1]);
+    occ +=    getLineOcc(x,y, [1,-1]);
+    occ +=    getLineOcc(x,y, [1, 0]);    
+    occ +=    getLineOcc(x,y, [1, 1]);
+    occ +=    getLineOcc(x,y, [0, 1]);
+    occ +=    getLineOcc(x,y, [-1,1]);
+    occ +=    getLineOcc(x,y, [-1,0]);
+    occ +=    getLineOcc(x,y, [-1,-1]);
+    //console.log(occ);
+    let seat = get(x, y);
+    if (seat == 'L' && occ == 0) {
+        return '#';
+    } else if (seat == '#' && occ >= 5) {
+        return 'L';
+    }
+    return seat;
+}
+
 function countOccupied() {
     let occ = 0;
     for (var y = 0; y < seats.length; y++) {
         occ += seats[y].filter(x => x == '#').length;
     }
     return occ;
+}
+
+function getLineOcc(x, y, delta) {
+    let line = [];
+    let row = y + delta[1];
+    let seat = x + delta[0];
+    let place = '';
+    while (get(seat, row) == '.') {
+        place = get(seat, row);
+        line.push(place);
+        seat += delta[0];
+        row += delta[1];
+    }
+    line.push(get(seat, row));
+    return line.filter(x => x == '#').length;
 }
 
 function getNeigbours(x, y) {
