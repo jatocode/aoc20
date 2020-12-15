8ae054e4f44466ded3b5cf4b502b2764549e421d
Tobias Jansson <Tobias.Jansson@prevas.se>
2020-12-15 10:53:25 +0100

    14, brute force del 2 ej färdig

--- 14.js
+++ 14.js
@@ -2,15 +2,20 @@
 const util = require('./util.js');
 
 let data = util.getInputTrim();
-let bitmask = '';
 
-let mem = decoderchip(data);
 
 // Del 1. Räkna summan
+let mem = decoderchip(data);
+console.log(mem.reduce((summa, value) => summa + calc(value), 0));
+
+// Del 2. Fucka runt med minnesaddresser
+let preparsed = [];
+mem = decoderchipv2(data);
 console.log(mem.reduce((summa, value) => summa + calc(value), 0));
 
 function decoderchip(data) {
     let mem = [];
+    let bitmask = '';
     data.forEach(line => {
         if (line.startsWith('mask')) {
             bitmask = line.match(/.* = (.*)/)[1];
@@ -28,20 +33,85 @@ function decoderchip(data) {
     return mem;
 }
 
-function applymask(mask, value) {
+function decoderchipv2(data) {
+    let mem = [];
+    let bitmask = '';
+    data.forEach((line, i) => {
+        if (line.startsWith('mask')) {
+            bitmask = line.match(/.* = (.*)/)[1];
+            console.log(i, bitmask, bitmask.split('').filter(x => x == 'X').length);
+        } else {
+            let parse = line.match(/mem\[(\d+)\] = (\d+).*/);
+            let paddr = parse[1];
+            let pvalue = parse[2];
+            let binval = dec2bin(parseInt(pvalue));
+            let value = pad(binval, 36);
+
+            let binaddr = dec2bin(parseInt(paddr));
+            let floater = applymask(bitmask, pad(binaddr, 36), true);
+            let floaters = floating([], floater);
+            floaters.forEach(binaddr => {
+                let addr = calc(binaddr);
+                if (mem[addr] == undefined) mem[addr] = pad(0, 36);
+                mem[addr] = value;
+            })
+        }
+    })
+    return mem;
+}
+
+function floating(list, value) {
+    let v = JSON.parse(JSON.stringify(value)).split('');
+    if (v.includes('X')) {
+        // if (preparsed.includes(v.join(''))) {
+        //     let p = preparsed(v.join(''));
+        //     console.log('cachehit');
+        //     p.forEach(el => {
+        //         floating(list, el);
+        //     });
+        // } else {
+           // console.log(preparsed);
+            for (var i = 0; i < v.length; i++) {
+                if (v[i] == 'X') {
+                    let v2 = JSON.parse(JSON.stringify(value)).split('');
+                    v2[i] = '0';
+                    //preparsed[v2.join('')] = 
+                    floating(list, v2.join(''));
+
+                    let v3 = JSON.parse(JSON.stringify(value)).split('');
+                    v3[i] = '1';
+                    //preparsed[v3.join('')] = 
+                    floating(list, v3.join(''));
+                }
+            }
+        }
+   // } else {
+        //let addr = calc(v.join(''));
+        let addr = v.join('');
+        if (!list.includes(addr)) {
+            //console.log(addr);
+            list.push(addr);
+        }
+    //}
+    return list;
+}
+
+function applymask(mask, value, memdecode = false) {
     let size = 36;
     let zeropad = pad(value, 36);
     result = '';
     for (var i = 0; i < size; i++) {
         switch (mask[i]) {
             case '0':
-                result += '0';
+                if (memdecode) result += zeropad[i];
+                else result += '0';
                 break;
             case '1':
                 result += '1';
                 break;
             case 'X':
-                result += zeropad[i];
+                if (memdecode) result += 'X';
+                else result += zeropad[i];
                 break;
         }
     }
